6ff75da9de5a40052c3623688616549e915807f6
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-26 20:53:37 -0600

    pass id test, fail email test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,9 @@ const Employee = require('../lib/Employee');
 
 // test to create an employee with name, id, and email
 test('creates an employee object', () => {
-    const employee = new Employee('Barry');
+    const employee = new Employee('Barry', '476', 'Barry@gmail.com');
   
     expect(employee.name).toBe('Barry');
+    expect(employee.id).toBe('476');
+    expect(employee.id).toBe('Barry@gmail.com');
   });
