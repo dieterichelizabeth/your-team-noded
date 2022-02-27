33e511539e7b63287bcd55ae38995cfb59e28a16
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-26 21:17:51 -0600

    create and pass manager class using employee test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,18 +1,10 @@
-const Manager = require ('../lib/Manager')
-const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
 
-// test to create an employee with name, id, and email
-test('creates an employee object', () => {
-    const employee = new Employee('Barry', '476', 'Barry@gmail.com');
+// test to create a Manager with name, id, and email
+test('create a manager', () => {
+    const manager = new Manager('Barry', '476', 'Barry@gmail.com');
   
-    expect(employee.name).toBe('Barry');
-    expect(employee.id).toBe('476');
-    expect(employee.email).toBe('Barry@gmail.com');
-  });
-
-// test for Office #
-test('get office #', () => {
-    const employee = new Employee('Barry', '476', 'Barry@gmail.com', '4001');
-  
-    expect(employee.office).toBe('4001');
+    expect(manager.name).toBe('Barry');
+    expect(manager.id).toBe('476');
+    expect(manager.email).toBe('Barry@gmail.com');
   });
