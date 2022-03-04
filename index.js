8820df58540456808b3cbbf9d3007f9d2bb50509
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-03 22:31:35 -0600

    update employee and manager to take inquirer input

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-// const Engineer = require('../lib/Engineer');
-// const Intern = require('../lib/Intern');
+
+// assigns anonymous HTML template function in page-template.js to the variable generatePage
+const generateHTML = require('./src/generateHTML');
 
 function Team() {
-    this.teamComplete = false;
     this.employee;
+    this.team = [];
 };
 
+// Start/welcome- enter the team name and move to add Manager
 Team.prototype.initializeTeam = function() {
     console.log(`
   <><><><><><><><><>~~~~~~~~~~~~~~~<><><><><><><><><>
@@ -32,6 +34,7 @@ Team.prototype.initializeTeam = function() {
     });
 };
 
+// Add your manager
 Team.prototype.addManager = function() {
     inquirer
       .prompt([
@@ -61,14 +64,16 @@ Team.prototype.addManager = function() {
       }
     ])
       .then(({ name, id, email, newOffice }) => {
-        this.employee = new Manager(name, id, email, newOffice);
-
+        console.log(this.employee);
+        this.employee = new Manager( name, id, email, newOffice );
+        this.team.push(this.employee);
+        console.table(this.employee.employeeDisplay());
         this.teamReview();
       }
     );
 };
 
-
+// Asks the user if they want to add another Employee
 Team.prototype.teamReview = function() {
   inquirer
   .prompt([
@@ -82,18 +87,22 @@ Team.prototype.teamReview = function() {
 ])
   .then (({isComplete}) => {
     console.log(isComplete);
+    // if yes for intern, move to addIntern
     if (isComplete === 'Intern'){
       this.addIntern();
     }
+    // if yes for engineer, move to addEngineer
     else if (isComplete === 'Engineer'){
       this.addEngineer();
     }
+    // if not, the team is complete(done)
     else {
       this.teamDone();
     }
   })
 };
 
+// Add an Intern
 Team.prototype.addIntern = function() {
   inquirer
       .prompt([
@@ -124,12 +133,14 @@ Team.prototype.addIntern = function() {
     ])
     .then(({ name, id, email, currentSchool }) => {
       this.employee = new Intern(name, id, email, currentSchool);
+      this.team.push(this.employee);
 
       this.teamReview();
     }
     );
 }
 
+// add an Engineer
 Team.prototype.addEngineer = function() {
   inquirer
       .prompt([
@@ -160,39 +171,19 @@ Team.prototype.addEngineer = function() {
     ])
     .then(({ name, id, email, githubUsername }) => {
       this.employee = new Engineer(name, id, email, githubUsername);
+      this.team.push(this.employee);
 
       this.teamReview();
     }
     );
 }
 
+// Team complete function
 Team.prototype.teamDone = function() {
   console.log("completed team!");
   // pass the information to a generate html function
+  console.log(this.team);
 }
 
 new Team().initializeTeam();
 module.exports = Team;
-
-
-
-/*
-Psuedocoding
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for my team members and their information
-THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated
-*/
---
+++ src/generateHTML.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+// const { resolve } = require('path');
+
+// function to write the file
+const generateHTML = () => {
+    console.log("made it");
+  };
+
+
+module.exports = { generateHTML };
