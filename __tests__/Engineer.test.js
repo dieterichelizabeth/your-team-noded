7f450a06bf24c53632339c1724f681b387eb6586
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-10 23:41:34 -0600

    updates classes

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,30 +1,12 @@
 const Engineer = require('../lib/Engineer');
 
 // test to create a Engineer with name, id, and email
-test('create a engineer', () => {
-    const engineer = new Engineer('Barry');
-  
-    engineer.getName('Barry');
-    engineer.getId('476');
-    engineer.getEmail('Barry@gmail.com');
+test('create a Engineer', () => {
+  const engineer = new Engineer('Barry', 476, 'Barry@gmail.com', 'theRock');
 
-    expect(engineer.name).toBe('Barry');
-    expect(engineer.id).toBe('476');
-    expect(engineer.email).toBe('Barry@gmail.com');
-  });
-
-// test to add Engineer github
-test('get Github', () => {
-    const engineer = new Engineer('Barry');
-    engineer.getGithub('octoBarry');
-  
-    expect(engineer.github).toBe('octoBarry');
-  });
-
-// test to change role from employee to engineer
-test('change employee role', () => {
-    const engineer = new Engineer('Barry');
-    engineer.getRole('Engineer');
-  
-    expect(engineer.role).toBe('Engineer');
-  });
+  expect(engineer.getName()).toBe('Barry');
+  expect(engineer.getId()).toBe(476);
+  expect(engineer.getEmail()).toBe('Barry@gmail.com');
+  expect(engineer.getGithub()).toBe('theRock');
+  expect(engineer.getRole()).toBe('Engineer');
+});
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,30 +1,12 @@
 const Intern = require('../lib/Intern');
 
 // test to create a Intern with name, id, and email
-test('create a intern', () => {
-  const intern = new Intern('Barry');
-  
-  intern.getName('Barry');
-  intern.getId('476');
-  intern.getEmail('Barry@gmail.com');
-  
-    expect(intern.name).toBe('Barry');
-    expect(intern.id).toBe('476');
-    expect(intern.email).toBe('Barry@gmail.com');
-  });
+test('create a Intern', () => {
+  const intern = new Intern('Barry', 476, 'Barry@gmail.com', 'UTA');
 
-// test to add Intern school
-test('get School', () => {
-    const intern = new Intern('Barry');
-    intern.getSchool('UT Austin');
-  
-    expect(intern.school).toBe('UT Austin');
-  });
-
-// test to change role from employee to intern
-test('change employee role', () => {
-    const intern = new Intern('Barry');
-    intern.getRole('Intern');
-  
-    expect(intern.role).toBe('Intern');
-  });
+  expect(intern.getName()).toBe('Barry');
+  expect(intern.getId()).toBe(476);
+  expect(intern.getEmail()).toBe('Barry@gmail.com');
+  expect(intern.getSchool()).toBe('UTA');
+  expect(intern.getRole()).toBe('Intern');
+});
